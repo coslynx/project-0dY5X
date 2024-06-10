c8ea9f4710e2bc6ab3269dc2d24443904e9d3695
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-06-10 18:22:47 +0530

    Added: `1v1-gaming-tournament-website/src/middleware/validationMiddleware.js`

---
+++ 1v1-gaming-tournament-website/src/middleware/validationMiddleware.js
@@ -0,0 +1,47 @@
+const validateUserRegistration = (req, res, next) => {
+  // Logic to validate user registration data
+};
+
+const validateTournamentCreation = (req, res, next) => {
+  // Logic to validate tournament creation data
+};
+
+const validateGameSelection = (req, res, next) => {
+  // Logic to validate game selection data
+};
+
+const validateBracketSystem = (req, res, next) => {
+  // Logic to validate bracket system data
+};
+
+const validateMatchmaking = (req, res, next) => {
+  // Logic to validate matchmaking data
+};
+
+const validateLiveStreaming = (req, res, next) => {
+  // Logic to validate live streaming data
+};
+
+const validateChatFunctionality = (req, res, next) => {
+  // Logic to validate chat functionality data
+};
+
+const validateLeaderboards = (req, res, next) => {
+  // Logic to validate leaderboards data
+};
+
+const validatePrizePool = (req, res, next) => {
+  // Logic to validate prize pool data
+};
+
+module.exports = {
+  validateUserRegistration,
+  validateTournamentCreation,
+  validateGameSelection,
+  validateBracketSystem,
+  validateMatchmaking,
+  validateLiveStreaming,
+  validateChatFunctionality,
+  validateLeaderboards,
+  validatePrizePool,
+};
